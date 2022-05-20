872b376d4f74e804c3d6a3658a2f2953a15c5660
Eray Gürbüz <eray58gurbuz@gmail.com>
2022-05-20 23:07:49 +0300

    C# resim çoklayıcı
    resim çoklayıcı

---
+++ ScreenSaver/ScreenSaver/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace ScreenSaver
+{
+    partial class FrmSS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // FrmSS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmSS";
+            this.ShowInTaskbar = false;
+            this.Text = "MY SCREEN SAVER";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FrmSS_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FrmSS_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FrmSS_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ScreenSaver/ScreenSaver/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenSaver
+{
+    public partial class FrmSS : Form
+    {
+        List<Image> BGimages = new List<Image>();
+        List<BritPic> britPics = new List<BritPic>();
+        Random random = new Random();
+
+        class BritPic
+        {
+            public int PicNum;
+            public float x;
+            public float y;
+            public float Speed;
+        }
+
+
+        public FrmSS()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmSS_KeyDown(object sender, KeyEventArgs e)
+        {
+            Close();    
+        }
+
+        private void FrmSS_Load(object sender, EventArgs e)
+        {
+            string[] images = System.IO.Directory.GetFiles("pics");
+            foreach (string image in images)
+            {
+                BGimages.Add(new Bitmap(image));
+            }
+            for (int i = 0; i < 3000
+                ; i++)
+
+            {
+                BritPic britPic = new BritPic();
+                britPic.PicNum=i%BGimages.Count;
+                britPic.x=random.Next(0,Width);
+                britPic.y=random.Next(0,Height);
+                britPics.Add(britPic);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void FrmSS_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (BritPic bp in britPics)
+            {
+                e.Graphics.DrawImage(BGimages[bp.PicNum], bp.x, bp.y);
+                bp.x -= 2;
+                if (bp.x<-250)
+                {
+                    bp.x = Width + random.Next(20,100);
+                }
+            }
+        }
+    }
+}
